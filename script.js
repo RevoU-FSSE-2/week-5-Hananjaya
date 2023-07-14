b113bdaac43b84e89951e2ad04fe260f499925cd
Hananjaya <hananjaya.zeroone@gmail.com>
2023-07-14 17:31:29 +0700

    add js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ hamburger.addEventListener('click', function(){
     navmobile.classList.toggle('d-none')
 })
 
-
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -31,4 +30,23 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+}
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
