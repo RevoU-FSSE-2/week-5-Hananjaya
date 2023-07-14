beafd64b668e36e87c9a506552e4934e4f93f1bf
Hananjaya <hananjaya.zeroone@gmail.com>
2023-07-14 15:48:12 +0700

    add slider

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const navmobile = document.getElementsByClassName('nav-mobile')[0]
 hamburger.addEventListener('click', function(){
     navmobile.classList.toggle('d-none')
 })
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
